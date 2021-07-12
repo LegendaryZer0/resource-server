e92583098c77785fdf729a51a255df2f966ee90d
Rifdar <assasinnyx@mail.ru>
2021-07-12 22:23:00 +0300

    simplified fake controller

--- src/main/java/ru/internship/api/controller/FakeController.java
+++ src/main/java/ru/internship/api/controller/FakeController.java
@@ -1,17 +1,20 @@
 package ru.internship.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.math.RandomUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.internship.api.model.User;
-@RequestMapping("/fake/resource")
+
 @RestController
+@Slf4j
 public class FakeController {
 
     @GetMapping("/getUser")
     public ResponseEntity<?> getResource() {
+        log.info("Upload info");
         return ResponseEntity.ok(User.builder().name("Пьер").surname("Кюри").id(RandomUtils.nextLong()*100).build());
     }
 }
