3b19ef64a201cdefa48020f2104b79a59282b089
Rifdar <assasinnyx@mail.ru>
2021-07-13 20:56:59 +0300

    added spring-native with graalVM

--- src/main/java/ru/internship/api/ResourceServerApplication.java
+++ src/main/java/ru/internship/api/ResourceServerApplication.java
@@ -7,9 +7,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ResourceServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ResourceServerApplication.class, args);
+
+
     }
 
 }
+
+
